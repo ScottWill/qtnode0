e5d791514118dcefbe51016451cdb7dfe15a51c3
Scott Will <scottdwill@gmail.com>
2025-12-13 19:37:01 -0600

    sput into features

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+#[cfg(feature = "oct")]
 mod oct;
-mod quad;
-
+#[cfg(feature = "oct")]
 pub use oct::{OctTreeMeta, OctTreeNode};
+
+#[cfg(feature = "quad")]
+mod quad;
+#[cfg(feature = "quad")]
 pub use quad::{QuadTreeMeta, QuadTreeNode};
